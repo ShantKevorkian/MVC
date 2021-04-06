d18b083601e83d3a1ae69ec52fb74ac8b8fad1a7
Shant Kevorkian <shantkevv@gmail.com>
2021-04-06 20:32:15 +0400

    First Commit

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 	$uri = $_SERVER['REQUEST_URI'];
     $uri = ltrim($uri, '/');
     $uri = explode('/', $uri);
-	$ctrl = '';
+	$control = '';
 	if(isset($uri[0]) && $uri[0] !== '') {
-		$ctrl = $uri[0];
+		$control = $uri[0];
 	
-		if (file_exists("Controllers/$ctrl.php")) {
-			require "Controllers/".$ctrl.".php";
-			if (class_exists($ctrl)) {
-				$class_name = $ctrl;
-				$ctrl_obj = new $class_name;
+		if (file_exists("Controllers/$control.php")) {
+			require "Controllers/".$control.".php";
+			if (class_exists($control)) {
+				$class_name = $control;
+				$control_obj = new $class_name;
 				if (isset($uri[1]) && $uri[1] !== '') {
 					$method = $uri[1];
-					if (method_exists($ctrl_obj, $method)) {
+					if (method_exists($control_obj, $method)) {
 						$params = array_slice($uri, 2);
-						call_user_func_array(array($ctrl_obj, $method), $params);
+						call_user_func_array(array($control_obj, $method), $params);
 					} 
 					else {
 						echo "The method '$uri[1]' does not exist in the class $uri[0]";
@@ -23,8 +23,8 @@
 				} 
 				else {
 					// If uri[1] is empty call the index method in the class if it exists
-					if (method_exists($ctrl_obj, "index")) {
-						$ctrl_obj->index();
+					if (method_exists($$control_obj, "index")) {
+						$$control_obj->index();
 					} 
 					else {
 						echo "The method index() does not exist in the class '$uri[0]'";
