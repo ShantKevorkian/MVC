db0b5f44735a5b35a6b10c61012ef97d91e01afe
Shant Kevorkian <shantkevv@gmail.com>
2021-04-12 18:03:33 +0400

    Modified and Added

---
+++ Views/register.php
@@ -0,0 +1,19 @@
+<div class = "container">
+    <div class="col-md-6 offset-md-3 bg-light border border-secondary rounded mt-5">
+        <h2 class="d-flex justify-content-center mt-2">Register</h2>
+        <form class = "m-5" action = "login" method = "POST">
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" name = "email" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name = "password">
+            </div>
+            <div class = "d-flex justify-content-center">
+                <button type="submit" class="btn btn-secondary mt-3">Register</button>
+            </div>
+        </form>
+    </div>
+</div
