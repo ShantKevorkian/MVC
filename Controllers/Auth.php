300ff1a6775a28f31b825dc8f88ab15dcf9d0664
Shant Kevorkian <shantkevv@gmail.com>
2021-04-15 09:46:49 +0400

    Modified and Added

--- Controllers/Auth.php
+++ Controllers/Auth.php
@@ -9,15 +9,25 @@
         public function register() {
             unset($_SESSION['userId']);
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+
+                if(empty($_POST['name'])) {
+                    $this->view->nameError = "Name required";
+                }
+                if(empty($_POST['password'])) {
+                    $this->view->passError = "Password required";
+                }
+                if(empty($_POST['email'])) {
+                    $this->view->emailError = "Email required";
+                }
+                else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $this->view->validateEmail = "Invalid email format";
+                }
+
+                else if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
                     $user = new User;
                     $user_exist = $user->userExist($_POST['email']);
-                    
-                    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                        $this->view->validateEmail = "Invalid email format";
-                    }
 
-                    else if($user_exist) {
+                    if($user_exist) {
                         $this->view->userError = "User already exists";
                     }
                     else {
@@ -30,17 +40,6 @@
                         }
                     }
                 }
-                else {
-                    if(empty($_POST['name'])) {
-                        $this->view->nameError = "Name required";
-                    }
-                    if(empty($_POST['email'])) {
-                        $this->view->emailError = "Email required";
-                    }
-                    if(empty($_POST['password'])) {
-                        $this->view->passError = "Password required";
-                    }
-                }
             }
             $this->view->render("register");
         }
@@ -49,30 +48,28 @@
         public function login() {
             unset($_SESSION['userId']);
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                if(!empty($_POST['email']) && !empty($_POST['password'])) {
+
+                if(empty($_POST['password'])) {
+                    $this->view->passError = "Password required";
+                }
+                if(empty($_POST['email'])) {
+                    $this->view->emailError = "Email required";
+                }
+                else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $this->view->validateEmail = "Invalid email format";
+                }
+
+                else if(!empty($_POST['email']) && !empty($_POST['password'])) {
                     $pass = md5($_POST['password']);
                     $user = new User;
                     $login_user = $user->login($_POST['email'], $pass);
 
-                    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                        $this->view->validateEmail = "Invalid email format";
-                    }
-
-                    else if(!$login_user) {
+                    if(!$login_user) {
                         $this->view->loginError = "Email or password incorrect";
                     }
                     else {
                         $_SESSION['userId'] =  $login_user['id'];
                         header("Location: /account");
-                    }
-                    
-                }
-                else {
-                    if(empty($_POST['email'])) {
-                        $this->view->emailError = "Email required";
-                    }
-                    if(empty($_POST['password'])) {
-                        $this->view->passError = "Password required";
                     }
                 }
             }
