4eb2af2bccfc31aee83d3571975247047bc81613
Shant Kevorkian <shantkevv@gmail.com>
2021-04-17 15:42:00 +0400

    Modified and Added

--- Views/friends.php
+++ Views/friends.php
@@ -5,13 +5,13 @@
             if($friend['avatar'] == NULL) {
                 $friend['avatar'] = "avatar.png";
             }
-            echo "<a class='col-sm-4' style = 'text-decoration: none;' href = '/account/user/".$friend['id']."'>
+            echo "<a class='col-sm-4 mb-3' style = 'text-decoration: none;' href = '/account/user/".$friend['id']."'>
                 <div class='bg-light border border-secondary rounded mt-2 border border-secondary'>
+                <div class = 'bg-secondary'>
+                    <h5 class='text-light d-flex justify-content-center p-3'>".$friend['name']."</h5>
+                </div>
                     <div class = 'd-flex justify-content-center'>
-                        <img src='/Public/Images/Avatars/".$friend['avatar']."' alt='avatar' class = 'rounded img-fluid' style = 'width: 50%;'>
-                        <div class='card-body'>
-                            <h5 class='card-title text-dark'>Name: ".$friend['name']."</h5>
-                        </div>
+                        <img src='/Public/Images/Avatars/".$friend['avatar']."' alt='avatar' class = 'img-fluid' >
                     </div>
                 </div>
             </a>";
