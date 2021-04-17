482e1a90d93452e68ba21ebb63c001ea7ad4218b
Shant Kevorkian <shantkevv@gmail.com>
2021-04-17 17:59:58 +0400

    Modified and Added

--- Views/account.php
+++ Views/account.php
@@ -6,13 +6,13 @@
                 <?php $this->userInfo['avatar'] = "avatar.png"; ?>
             <?php endif; ?>
             <div class='col-sm-3'>
-                    <div class='bg-light border border-secondary rounded mt-2'>
-                        <h4 class = 'd-flex justify-content-center p-2 bg-secondary text-light'><?=$this->userInfo['name']?></h4>
-                        <div class = 'd-flex justify-content-center'>
-                            <img src='/Public/Images/Avatars/<?=$this->userInfo['avatar']?>' alt='avatar' class = 'img-fluid'>
-                        </div>
+                <div class='bg-light border border-secondary rounded mt-2'>
+                    <h4 class = 'd-flex justify-content-center p-2 bg-secondary text-light'><?=$this->userInfo['name']?></h4>
+                    <div class = 'd-flex justify-content-center'>
+                        <img src='/Public/Images/Avatars/<?=$this->userInfo['avatar']?>' alt='avatar' class = 'img-fluid'>
                     </div>
                 </div>
+            </div>
         <?php else: ?>
             <div class='col-sm-3'>
                     <div class='bg-light border border-secondary rounded mt-2'>
