e79d6e1ebd2554d2e6b2032936cbb283a5c6fc54
Shant Kevorkian <shantkevv@gmail.com>
2021-04-20 13:58:29 +0400

    Modified and Added

--- Views/friends.php
+++ Views/friends.php
@@ -5,16 +5,22 @@
             <?php if($friend['avatar'] == NULL): ?>
                 <?php $friend['avatar'] = "avatar.png"; ?>
             <?php endif; ?>
-            <a class='col-sm-4 mb-3' style = 'text-decoration: none;' href = '/account/user/<?=$friend['id']?>'>
-                <div class='bg-light border border-secondary rounded mt-2 border border-secondary'>
+            <div class = "col-sm-4 mb-3">
+                <div class='bg-light border border-secondary rounded mt-2'>
                     <div class = 'bg-secondary'>
-                        <h5 class='text-light d-flex justify-content-center p-3'><?=$friend['name']?></h5>
+                        <h5 class='text-light p-3'><?=$friend['name']?>
+                        <span class = "float-right">
+                            <a href = "/account/chat/<?=$friend['id']?>" style='font-size: 30px; text-decoration: none;' class = 'far text-light'>&#xf075;</a>
+                        </span>
+                        </h5>
                     </div>
-                    <div class = 'd-flex justify-content-center'>
-                        <img src='/Public/Images/Avatars/<?=$friend['avatar']?>' alt='avatar' class = 'img-fluid' >
-                    </div>
+                    <a class='' style = 'text-decoration: none;' href = '/account/user/<?=$friend['id']?>'>
+                        <div class = 'd-flex justify-content-center'>
+                            <img src='/Public/Images/Avatars/<?=$friend['avatar']?>' alt='avatar' class = 'img-fluid' >
+                        </div>
+                    </a>
                 </div>
-            </a>
+            </div>
         <?php endforeach; ?>
     </div>
 </div>
